152b0ffa727dbd63c5faf4f2a761abfe85f991b4
kruemelmann <kruemelmannlp@gmail.com>
2022-04-11 09:45:36 +0200

    fix id error on ui

--- ui/komodo/src/components/PDFLoader/PDFLoader.js
+++ ui/komodo/src/components/PDFLoader/PDFLoader.js
@@ -1,4 +1,4 @@
-import { React, useRef } from 'react';
+import { React, useRef, useEffect } from 'react';
 import * as pdfjsLib from "pdfjs-dist/build/pdf";
 import { pdfjsWorker } from "pdfjs-dist/build/pdf.worker.entry";
 import './PDFLoader.css';
@@ -8,35 +8,47 @@ const client = new W3CWebSocket('ws://localhost:9090/ws/pdf');
 
 const PDFLoader = () => {
     var url = 'http://localhost:9090/pdf';
-    const canvasRef = useRef();
-
     const loadpdf = () => {
+        var canvas_container = document.getElementById('canvas-container');
+        canvas_container.innerHTML = '';
+
         var loadingTask = pdfjsLib.getDocument(url);
         loadingTask.promise.then(function(pdf) {
-            pdf.getPage(1).then(function(page) {
-                var viewport = page.getViewport(
-                    {scale: 2, }
-                );
+            var page_count = pdf.numPages;
+            for (let i = 1; i <= page_count; i++) {
+                pdf.getPage(i).then(function(page) {
+                    var viewport = page.getViewport(
+                        {scale: 2, }
+                    );
+                    var outputScale = window.devicePixelRatio || 1;
 
-                var outputScale = window.devicePixelRatio || 1;
-                var canvas = document.getElementById('the-canvas');
-                canvas.width = Math.floor(viewport.width * outputScale);
-                canvas.height = Math.floor(viewport.height * outputScale);
-                canvas.style.width = Math.floor(viewport.width) + "px";
-                canvas.style.height =  Math.floor(viewport.height) + "px";
-                var context = canvas.getContext('2d');
+                    let node = document.createElement("canvas");
+                    node.setAttribute("id", "the-canvas"+i);
+                    canvas_container.appendChild(node);
 
-                var transform = outputScale !== 1 ? [outputScale, 0, 0, outputScale, 0, 0] : null;
-                var renderContext = {
-                    canvasContext: context,
-                    transform: transform,
-                    viewport: viewport
-                };
-                page.render(renderContext);
-            });
+                    var canvas = document.getElementById('the-canvas'+i);
+
+                    canvas.width = Math.floor(viewport.width * outputScale);
+                    canvas.height = Math.floor(viewport.height * outputScale);
+                    canvas.style.width = Math.floor(viewport.width) + "px";
+                    canvas.style.height =  Math.floor(viewport.height) + "px";
+                    var context = canvas.getContext('2d');
+
+                    var transform = outputScale !== 1 ? [outputScale, 0, 0, outputScale, 0, 0] : null;
+                    var renderContext = {
+                        canvasContext: context,
+                        transform: transform,
+                        viewport: viewport
+                    };
+                    page.render(renderContext);
+
+                });
+            }
         });
     }
-    loadpdf()
+    useEffect(()=>{
+        loadpdf()
+    }, [loadpdf])
 
     client.onopen = () => {
       console.log('WebSocket Client Connected');
@@ -47,8 +59,7 @@ const PDFLoader = () => {
 
     return (
         <>
-            <div class="canvas-container">
-                <canvas id="the-canvas" />
+            <div id="canvas-container">
             </div>
         </>
     );
