819f57d6ac898548390187f55369321354dd372e
kruemelmann <kruemelmannlp@gmail.com>
2022-05-02 05:09:50 +0200

    fixed wrong page order when serving to the frontend

--- ui/komodo/src/components/PDFLoader/PDFLoader.js
+++ ui/komodo/src/components/PDFLoader/PDFLoader.js
@@ -13,39 +13,44 @@ const PDFLoader = () => {
         canvas_container.innerHTML = '';
 
         var loadingTask = pdfjsLib.getDocument(url);
-        loadingTask.promise.then(function(pdf) {
-            var page_count = pdf.numPages;
-            for (let i = 1; i <= page_count; i++) {
-                pdf.getPage(i).then(function(page) {
-                    var viewport = page.getViewport(
-                        {scale: 2, }
-                    );
-                    var outputScale = window.devicePixelRatio || 1;
-
-                    let node = document.createElement("canvas");
-                    node.setAttribute("id", "the-canvas"+i);
-                    canvas_container.appendChild(node);
-
-                    var canvas = document.getElementById('the-canvas'+i);
-
-                    canvas.width = Math.floor(viewport.width * outputScale);
-                    canvas.height = Math.floor(viewport.height * outputScale);
-                    canvas.style.width = Math.floor(viewport.width) + "px";
-                    canvas.style.height =  Math.floor(viewport.height) + "px";
-                    var context = canvas.getContext('2d');
-
-                    var transform = outputScale !== 1 ? [outputScale, 0, 0, outputScale, 0, 0] : null;
-                    var renderContext = {
-                        canvasContext: context,
-                        transform: transform,
-                        viewport: viewport
-                    };
-                    page.render(renderContext);
-
-                });
+        loadingTask.promise.then(async function(pdf) {
+            for (let i = 1; i <= pdf.numPages; i++) {
+               let a = await pdf.getPage(i).then(renderpage);
             }
         });
     }
+
+    let page_num = 0
+    const renderpage = (page) => {
+        var canvas_container = document.getElementById('canvas-container');
+        var viewport = page.getViewport(
+            {scale: 2, }
+        );
+        var outputScale = window.devicePixelRatio || 1;
+
+        let node = document.createElement("canvas");
+        node.setAttribute("id", "the-canvas"+page_num);
+        canvas_container.appendChild(node);
+
+        var canvas = document.getElementById('the-canvas'+page_num);
+
+        canvas.width = Math.floor(viewport.width * outputScale);
+        canvas.height = Math.floor(viewport.height * outputScale);
+        canvas.style.width = Math.floor(viewport.width) + "px";
+        canvas.style.height =  Math.floor(viewport.height) + "px";
+        var context = canvas.getContext('2d');
+
+        var transform = outputScale !== 1 ? [outputScale, 0, 0, outputScale, 0, 0] : null;
+        var renderContext = {
+            canvasContext: context,
+            transform: transform,
+            viewport: viewport
+        };
+        page.render(renderContext);
+        page_num++;
+
+    }
+
     useEffect(()=>{
         loadpdf()
     }, [loadpdf])
