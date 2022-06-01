772d1eeff1d91b3a76962f431796288240f1a5d3
kruemelmann <kruemelmannlp@gmail.com>
2022-06-01 18:03:32 +0200

    embedding of the frontend now works as expected

--- embed.go
+++ embed.go
@@ -2,5 +2,5 @@ package komodo
 
 import "embed"
 
-//go:embed bazel-out/*/bin/ui/komodo/build
+//go:embed */*/*/ui/komodo/build
 var FrontendFS embed.FS
--- pkg/web/server.go
+++ pkg/web/server.go
@@ -1,9 +1,11 @@
 package web
 
 import (
+	"embed"
 	"io/fs"
 	"log"
 	"net/http"
+	"path/filepath"
 	"time"
 
 	"github.com/gorilla/handlers"
@@ -17,7 +19,13 @@ func StartServer(host, port, filename string) {
 	r.HandleFunc("/ws/pdf", BuildPdfWebsocket())
 	r.HandleFunc("/pdf", ReadFileHandler(filename))
 
-	feRoot, _ := fs.Sub(komodo.FrontendFS, "ui/komodo/build")
+	//search for the frontend files in the filesystem
+	matches, _ := fs.Glob(komodo.FrontendFS, "*/*/*/ui/komodo/build")
+	if len(matches) != 1 {
+		panic("missmatching count of frontend build files in FS")
+	}
+
+	feRoot, _ := fs.Sub(komodo.FrontendFS, matches[0])
 	buildHandler := http.FileServer(http.FS(feRoot))
 	r.PathPrefix("/").Handler(buildHandler)
 
@@ -33,4 +41,27 @@ func StartServer(host, port, filename string) {
 
 	log.Println("Server started on " + host + ":" + port)
 	log.Fatal(srv.ListenAndServe())
+}
+
+func getAllFilenames(fs *embed.FS, path string) (out []string, err error) {
+	if len(path) == 0 {
+		path = "."
+	}
+	entries, err := fs.ReadDir(path)
+	if err != nil {
+		return nil, err
+	}
+	for _, entry := range entries {
+		fp := filepath.Join(path, entry.Name())
+		if entry.IsDir() {
+			res, err := getAllFilenames(fs, fp)
+			if err != nil {
+				return nil, err
+			}
+			out = append(out, res...)
+			continue
+		}
+		out = append(out, fp)
+	}
+	return
 }
